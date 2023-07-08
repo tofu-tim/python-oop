e3b633f12efc75726d08c92f33dad65b349c0d03
Timothy Wilger <47797775+tofu-tim@users.noreply.github.com>
2023-07-07 23:52:04 -0500

    Add files via upload

--- user.py
+++ user.py
@@ -1,8 +1,21 @@
-class User:		
-    def __init__(self):
-        self.first_name = "Tony"
-        self.last_name = "Hawk"
-        self.age = 55
+class Shoe:
+    def __init__(self, brand, shoe_type, price):
+        self.brand = brand
+        self.type = shoe_type
+        self.price = price
+        self.in_stock = True
 
-user_tony = User()
-print(user_tony.first_name)
+skater_shoe = Shoe("Vans", "Low-top Trainers", 59.99)
+dress_shoe = Shoe("Jack & Jill Bootery", "Ballet Flats", 29.99)
+print(skater_shoe.type)
+print(dress_shoe.type)
+
+# Ninja Challenges!
+# Open this code on the Trace website to get a better view of all the variables and their attributes.
+# Make a new instance of a shoe
+climbing_shoe = Shoe("Black Diamond", "Climbing Shoe", 74.96)
+# Update the in_stock attribute to False
+
+climbing_shoe.in_stock = False
+
+print(climbing_shoe.in_stock)
