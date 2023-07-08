20c6b35ec9ee3873afb44a01ddb38823ae4d8909
Timothy Wilger <47797775+tofu-tim@users.noreply.github.com>
2023-07-07 23:56:51 -0500

    Add files via upload

--- user.py
+++ user.py
@@ -1,21 +1,31 @@
 class Shoe:
+
     def __init__(self, brand, shoe_type, price):
         self.brand = brand
         self.type = shoe_type
         self.price = price
         self.in_stock = True
+    
+    # Takes a float/percent as an argument and reduces the
+    # price of the item by that percentage.
+    def on_sale_by_percent(self, percent_off):
+        self.price = self.price * (1-percent_off)
+    
+    # Returns a total with tax added to the price.
+    def total_with_tax(self, tax_rate):
+        tax = self.price * tax_rate
+        total = self.price + tax
+        return total
+    
+    # Reduces the price by a fixed dollar amount.
+    def cut_price_by(self, amount):
+    	if amount < self.price:
+        	self.price -= amount
+    	else:
+    		print("Price deduction too large.")
 
-skater_shoe = Shoe("Vans", "Low-top Trainers", 59.99)
-dress_shoe = Shoe("Jack & Jill Bootery", "Ballet Flats", 29.99)
-print(skater_shoe.type)
-print(dress_shoe.type)
-
-# Ninja Challenges!
-# Open this code on the Trace website to get a better view of all the variables and their attributes.
-# Make a new instance of a shoe
-climbing_shoe = Shoe("Black Diamond", "Climbing Shoe", 74.96)
-# Update the in_stock attribute to False
-
-climbing_shoe.in_stock = False
-
-print(climbing_shoe.in_stock)
+# Create some shoes. Call some methods on those shoes. Print your shoe's attributes..
+my_shoe = Shoe("Converse", "Low-tops", 36.00)
+print(my_shoe.total_with_tax(0.05))
+my_shoe.cut_price_by(10)
+print(my_shoe.price)
