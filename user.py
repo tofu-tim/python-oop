74a4316835b5445b7deb354396e88f0bb818efe6
Timothy Wilger <47797775+tofu-tim@users.noreply.github.com>
2023-07-08 00:09:19 -0500

    Add files via upload

--- user.py
+++ user.py
@@ -1,5 +1,19 @@
 class User:
-
     def __init__(self, first_name, last_name, email, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.age = age
         self.is_rewards_member = False
         self.gold_card_points = 0
+
+    def display_info(self):
+        print("First name: " + self.first_name)
+        print("Last name: " + self.last_name)
+        print("Email: " + self.email)
+        print("Age: " + str(self.age))
+        if self.is_rewards_member == False:
+            print("Rewards member status: inactive")
+        else:
+            print("Rewards member status: Active")
+            print("Gold Card points: " + str(self.gold_card_points))
