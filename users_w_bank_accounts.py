951d32398f0fbc6f19a40a2aba4567b89e940ba2
Timothy Wilger <47797775+tofu-tim@users.noreply.github.com>
2023-07-09 22:28:44 -0500

    Add files via upload

--- users_w_bank_accounts.py
+++ users_w_bank_accounts.py
@@ -40,6 +40,25 @@ class BankAccount:
             self.balance += self.balance * self.int_rate
             print("Balance with interest: ", self.balance)
         return self
+    
+class User:
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+        self.account = BankAccount(int_rate=0.02, balance=0)
+    
+    def make_withdrawal(self, amount):
+        self.account.withdraw(amount)
+        return self
+    
+    def display_user_balance(self):
+        self.account.display_account_info()
+    
+    def make_deposit(self, amount):
+        self.account.deposit(amount)
+        return self
+
+
 
 user1 = BankAccount(1.05, 1000000)
 user2 = BankAccount(1.10, 42000)
