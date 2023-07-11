26067c4eae4ee2bd8d97583920950e3f4b61fe6e
Timothy Wilger <47797775+tofu-tim@users.noreply.github.com>
2023-07-10 19:23:23 -0500

    Add files via upload

--- basketball_dictionaries_walkthrough.py
+++ basketball_dictionaries_walkthrough.py
@@ -6,7 +6,7 @@ class Player:
         self.team = player_info["team"]
 
     def __repr__(self):
-        return "Player: {}, {} y/o, position: {}, team: {}".format(
+        return "Player: {}\n{} y/o\nPosition: {}\nTeam: {}\n".format(
             self.name, self.age, self.position, self.team)
 
 kevin = {
@@ -33,3 +33,52 @@ player_jason = Player(jason)
 player_kyrie = Player(kyrie)
 
 print(player_kevin)
+print(player_jason)
+print(player_kyrie)
+
+players = [
+    {
+    	"name": "Kevin Durant", 
+    	"age":34, 
+    	"position": "small forward", 
+    	"team": "Brooklyn Nets"
+    },
+    {
+    	"name": "Jason Tatum", 
+    	"age":24, 
+    	"position": "small forward", 
+    	"team": "Boston Celtics"
+    },
+    {
+    	"name": "Kyrie Irving", 
+    	"age":32, "position": "Point Guard", 
+    	"team": "Brooklyn Nets"
+    },
+    {
+    	"name": "Damian Lillard", 
+    	"age":33, "position": "Point Guard", 
+    	"team": "Portland Trailblazers"
+    },
+    {
+    	"name": "Joel Embiid", 
+    	"age":32, "position": "Power Foward", 
+    	"team": "Philidelphia 76ers"
+    },
+    {
+    	"name": "Mario McCoy", 
+    	"age":16, 
+    	"position": "Wide Reciever", 
+    	"team": "Primitive Skate"
+    }
+]
+
+# for loop over list of dictionaries
+# each loop will use current dictionary info to create a new player
+
+new_team_roster = []
+
+for player_dict in players:
+    player = Player(player_dict)
+    new_team_roster.append(player)
+
+print(new_team_roster)
